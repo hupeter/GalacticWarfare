a378aa53fba63cd6efe48bcf5f6d120773d52d74
Peter Hu <peter.s1.hu@gmail.com>
2021-04-23 20:51:25 -0700

    hello

---
+++ desktop/src/com/redlotus/galacticwarfare/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.redlotus.galacticwarfare.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.redlotus.galacticwarfare.HelloWorld;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HelloWorld(), config);
+	}
+}
